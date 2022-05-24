ea51043738d311447e88a3640d9c1d29a653dac5
kei-123456789 <104549980+kei-123456789@users.noreply.github.com>
2022-05-24 11:18:13 +0900

    メイン画像など

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2022_05_23_053439) do
 
   create_table "prefectures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
