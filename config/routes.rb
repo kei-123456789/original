ef71cc3d8b6895974eeb4ca9a6dc31cdbdb1d243
kei-123456789 <104549980+kei-123456789@users.noreply.github.com>
2022-05-24 20:01:02 +0900

    一旦、ビュー設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'prefectures#index'
   resources :prefectures , only: :show
-  resources :contents , only: :index
+  resources :contents , only: [:new, :create]
 end
