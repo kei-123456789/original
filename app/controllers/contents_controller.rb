d37dcd10f0d416ef7601122c22e333ae41f441e5
kei-123456789 <104549980+kei-123456789@users.noreply.github.com>
2022-05-25 15:28:49 +0900

    contentのnewビューとコントローラー

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,6 +1,15 @@
 class ContentsController < ApplicationController
   def new
+    @content = Content.new
   end
   def create
+    @content = Content.new(content_params)
   end
+
+  private
+
+  def content_params
+    params.require(:content).permit(:explain, :visit, :prefecture_id, :image).merge(user_id: current_user.id)
+  end
+
 end
