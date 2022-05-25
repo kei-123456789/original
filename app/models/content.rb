ed0429ac6d80800e939d52ff8357560b92c42392
kei-123456789 <104549980+kei-123456789@users.noreply.github.com>
2022-05-25 15:32:05 +0900

    active storage

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,6 +1,6 @@
 class Content < ApplicationRecord
   belongs_to :user
   belongs_to :prefecture
-  has_many_attached :image
+  has_one_attached :image
   validates :explain, :visit, :image, presence: true
 end
