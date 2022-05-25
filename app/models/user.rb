90bd15e72e9df65ead32c85a15fd5d0adefde3e3
kei-123456789 <104549980+kei-123456789@users.noreply.github.com>
2022-05-25 11:17:59 +0900

    ユーザー新規登録機能完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
   has_many :contents
 end
