ff1638074b7f9db5d4b85608aec3c74743826934
kei-123456789 <104549980+kei-123456789@users.noreply.github.com>
2022-06-14 17:33:18 +0900

    値を取得し表示まで

--- app/javascript/weather.js
+++ app/javascript/weather.js
@@ -1,15 +1,6 @@
 'use strict';
 
-// geolocation
-function success(pos) {
-  ajaxRequest(pos.coords.latitude, pos.coords.longitude);
-}
-
-function fail(error) {
-  alert('位置情報の取得に失敗しました。エラーコード:' + error.code);
-}
-
-navigator.geolocation.getCurrentPosition(success, fail);
+ajaxRequest(43.046848, 141.322114);
 
 // UTCをミリ秒に
 function utcToJSTime(utcTime) {
@@ -20,7 +11,6 @@ function utcToJSTime(utcTime) {
 function ajaxRequest(lat, long) {
   const url = 'https://api.openweathermap.org/data/2.5/forecast';
   const appId = 'af04c9d9fbeeb75ac003c77f6b7f4018';
-
 
   $.ajax({
     url: url,
@@ -36,8 +26,7 @@ function ajaxRequest(lat, long) {
     console.log(data);
 
     // 都市名、国名
-    console.log('都市名:' + data.city.name);
-    console.log('国名:' + data.city.country);
+    $('#place').text(data.city.name + ',' + data.city.country);
 
     // 天気予報データ
     data.list.forEach(function(forecast, index) {
@@ -48,12 +37,31 @@ function ajaxRequest(lat, long) {
       const min = String(dateTime.getMinutes()).padStart(2, '0');
       const temperature = Math.round(forecast.main.temp);
       const description = forecast.weather[0].description;
-      const iconPath = `images/${forecast.weather[0].icon}.svg`;
+      const iconPath = `/assets/${forecast.weather[0].icon}.svg`;
 
-      console.log('日時:' + `${month}/${date} ${hours}:${min}`);
-      console.log('気温' + temperature);
-      console.log('天気' + description);
-      console.log('画像パス' + iconPath);
+    // 現在の天気とそれ以外で出力を変える
+    if(index === 0) {
+      const currentWeather = `
+      <div class="icon"><img src="${iconPath}"></div>
+      <div class="info">
+        <p>
+          <span class="description">現在の天気:${description}</span>
+          <span class="temp">${temperature}</span>°c
+        </p>
+      </div>`;
+      $('#weather').html(currentWeather);
+    } else {
+      const tableRow = `
+      <tr>
+        <td class="info">
+          ${month}/${date} ${hours}:${min}
+        </td>
+        <td class="icon"><img src="${iconPath}"></td>
+        <td><span class="description">${description}</span></td>
+        <td><span class="temp">${temperature}</span></td>
+      </tr>`;
+      $('#forecast').append(tableRow);
+    }
     });
   })
   .fail(function() {
