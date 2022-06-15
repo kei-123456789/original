9b4449c3e9908df4aae9400999fe84b2494876ad
kei-123456789 <104549980+kei-123456789@users.noreply.github.com>
2022-06-15 11:48:30 +0900

    都道府県ごとに設定

--- app/models/prefecture.rb
+++ app/models/prefecture.rb
@@ -2,7 +2,7 @@ class Prefecture < ActiveHash::Base
 
   self.data = [
     { id: 1, name: '---' },
-    { id: 2, name: '北海道' },
+    { id: 2, name: '北海道'},
     { id: 3, name: '青森' },
     { id: 4, name: '秋田' },
     { id: 5, name: '岩手' },
